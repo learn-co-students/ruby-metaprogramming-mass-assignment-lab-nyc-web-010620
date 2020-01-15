25a89ac70dbfc5325ad8c1888d0a5bcb9010b9f2
alex.szeto <alex.szeto.a@gmail.com>
2020-01-15 14:12:11 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,7 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width 
+  def initialize(arguements)
+    arguements.each{|name, value| self.send(("#{name}="), value)}
+  end
 end
